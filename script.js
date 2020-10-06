a9adda2a05e77b8ef23eb33aa65a9721a86c43e1
celestine <nwabuisicelestine@gmail.com>
2020-10-06 16:30:35 +0100

    created a function that ends the game

--- script.js
+++ script.js
@@ -2,8 +2,21 @@ var character = document.getElementById('character')
 var block = document.getElementById('block');
 
 function jump() {
-    character.classList.add('animate');
+    if (character.classList != 'animate') {
+        character.classList.add("animate");
+    }
     setTimeout(() => {
         character.classList.remove('animate')
     }, 900);
 }
+
+var checkDead = setInterval(() => {
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
+    if (blockLeft < 20 && blockLeft > 0 && characterTop >= 530) {
+        block.style.animation = 'none';
+        block.style.display = 'none';
+        character.style.display = 'none';
+        alert('Game Over!!! You Lose....');
+    }
+}, 10);
